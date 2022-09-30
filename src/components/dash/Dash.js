2f5e55f4a71cc5a5c581e1608a3400600c5e5c83
LuisSilva11 <lssilva07@misena.edu.co>
2022-09-30 18:54:06 -0500

    dash y register

--- src/components/dash/Dash.js
+++ src/components/dash/Dash.js
@@ -1,112 +1,121 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import señora from "../../img/señora.jpg";
 import logodash from "../../img/Hotelia horizontal blanco.svg";
 
 function Dash() {
     return (
-        <div>
+        <div class="bodydash">
             <nav class="navdash">
-                <div class="fondito">
-                    <div class="loguito">
-                        <img class="logocompleto" src={logodash} alt="" />
-                        <div class="datos">
-                            <h4>Angie Vargas</h4>
-                            <p class="parra">angievargas2@gmail.com</p>
-                        </div>
+                <div class="logo">
+                    <img src={logodash} />
+                    <div class="container-username">
+                        <h3>Angie Vargas</h3>
+                        <p>Angievargas2@gmail.com</p>
                     </div>
                 </div>
-                <div class="options">
-                    <Link href="#" class="items"><i class="fa-solid fa-bed" ></i> Habitaciones</Link><br />
-                    <Link href="#" class="items"><i class="fa-solid fa-bell-concierge" ></i> Reservas</Link><br />
-                    <Link href="#" class="items"><i class="fa-solid fa-bed" ></i> Habitaciones</Link><br />
-                    <Link href="#" class="items"><i class="fa-solid fa-bell-concierge" ></i> Reservas</Link><br />
+                <div class="container-lateral-bar">
+                    <div class="icons">
+                        <div><i class="fa-solid fa-bed"></i><Link href="#">Habitaciones</Link></div>
+                        <div><i class="fa-solid fa-bell-concierge"></i><Link href="#">Reservas</Link></div>
+                        <div class="icon-active"> <i class="fa-solid fa-bed"></i><Link href="#">Habitaciones</Link></div>
+                        <div><i class="fa-solid fa-bell-concierge"></i><Link href="#">Habitaciones</Link></div>
+                    </div>
                 </div>
-                <Link to="/" class="item"><button class="salir"><i class="fa-solid fa-arrow-right-from-bracket"></i>Salir</button></Link>
+                <div class="container-exit">
+                    <i class="fa-solid fa-right-from-bracket"></i><Link href="/">Cerrar sesión</Link>
+                </div>
             </nav>
-            <section class="information">
-                <div class="information-image">
-                    <img src={señora} alt="" />
+            <section class="container-dashboard">
+                <div class="container-message">
+                    <h1>Bienvenido, Angie Vargas</h1>
                 </div>
-                <div class="information-title">
-                    <h2>Bienvenida, Angie Vargas</h2>
-                </div>
-                <div class="information-text">
-                    <div class="tipoid">
-                        <label>Tipo de documento</label>
-                        <br />
-                        <input placeholder="Cédula de ciudadanía" type="text" />
-                        <div class="id">
-                            <label for="">Numero de documento</label>
-                            <br />
-                            <input placeholder="1025417456" type="text" />
+
+                <div class="container-information">
+                    <div class="container-photo">
+                        <div class="container-image"><img src={señora} /></div>
+                        <div class="icons-two">
+                            <i class="fa-solid fa-file-export"></i>
                         </div>
-                        <div class="name">
-                            <label for="">Nombres</label>
-                            <br />
-                            <input placeholder="Angie Camila" type="text" />
+                        <div class="icons-three">
+                            <i class="fa-solid fa-file-export"></i>
+                            <i class="fa-solid fa-file-image"></i>
                         </div>
-                        <div class="last">
-                            <label for="">Apellidos</label>
-                            <br />
-                            <input placeholder="Vargas Pinzón" type="text" />
-                        </div>
-                        <div class="date">
-                            <label for="">Fecha nacimiento</label>
-                            <br />
-                            <input type="date" />
-                        </div>
-                        <div class="country">
-                            <label for="">País de origen</label>
-                            <br />
-                            <input placeholder="Colombia" type="text" />
-                        </div>
-                        <div class="form-check">
-                            <label for="">Genero</label>
-                            <div class="genero">
-                                <div class="men">
-                                    <input type="radio" class="hombre" />
-                                    <label for="hombre">Hombre</label>
+                        <a href="#">Cambiar contraseña</a>
+                    </div>
+
+                    <form class="container-form">
+                        <div class="container-columns">
+                            <div class="subcontainer-columns">
+                                <div>
+                                    <label>Tipo de documento</label>
+                                    <input type="text" value="Cedula de ciudadania" />
                                 </div>
-                                <div class="woman">
-                                    <input type="radio" class="mujer" checked />
-                                    <label for="mujer">Mujer</label>
+                                <div>
+                                    <label>Fecha de nacimiento</label>
+                                    <input type="date" value="1976-08-10" />
                                 </div>
-                                <input type="radio" class="otro" />
-                                <label for="otro">Otro</label>
+                                <div>
+                                    <label>Nombre</label>
+                                    <input type="text" value="Angie Camila" />
+                                </div>
+                                <div>
+                                    <label>Género</label>
+                                    <div id="container-radio">
+                                        <input type="radio" /><label>Mujer</label>
+                                        <input type="radio" /><label>Hombre</label>
+                                        <input type="radio" /><label>Otro</label>
+                                    </div>
+                                </div>
+                                <div>
+                                    <label>Email</label>
+                                    <input type="email" value="Angievargas2@gmail.com" />
+                                </div>
+
+                                <div class="container-buttons">
+                                    <button type="submit">Actualizar</button>
+                                </div>
+
+                                <div class="container-password">
+                                    <div>
+                                        <label>contraseña</label>
+                                        <input type="password" />
+                                    </div>
+                                </div>
+
+                                <div class="container-buttons">
+                                    <button type="submit">Cambiar</button>
+                                </div>
+                            </div>
+
+                            <div class="subcontainer-columns">
+                                <div>
+                                    <label>Número de documento</label>
+                                    <input type="number" value="1025417456" />
+                                </div>
+                                <div>
+                                    <label>Apellido</label>
+                                    <input type="text" value="Vargas Pinzon" />
+                                </div>
+                                <div>
+                                    <label>País de origen</label>
+                                    <input type="text" value="Colombia" />
+                                </div>
+                                <div>
+                                    <label>Telefono</label>
+                                    <input type="number" value="3135207460" />
+                                </div>
+
+
+                                <div class="subcontainer-password">
+                                    <div>
+                                        <label>Confirmar contraseña</label>
+                                        <input type="password" />
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
-                        <div class="cel">
-                            <label for="">Telefono de contacto</label>
-                            <br />
-                            <input placeholder="3125207460" type="text" />
-                        </div>
-                        <div class="mail">
-                            <label for="">Email</label>
-                            <br />
-                            <input placeholder="angievargas2@gmail.com" type="text" />
-                        </div>
-                        <div class="actu">
-                            <button class="cambiar">Actualizar</button>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="pass">
-                            <label for="">Contraseña</label>
-                            <br />
-                            <input type="password" />
-                        </div>
-                        <div class="confpass">
-                            <label for="">Confirmar contraseña</label>
-                            <br />
-                            <input type="password" />
-                        </div>
-                        <div class="camb">
-                            <button class="cambiar">Cambiar</button>
-                        </div>
-                        <div class="cambpass">
-                            <button class="cambiarpass">Cambiar Contraseña</button>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </section>
         </div>
