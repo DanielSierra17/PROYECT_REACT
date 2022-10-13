7977499a8a98250f283237e61bf488e61df822ae
DanielSierra17 <dfsierra898@misena.edu.co>
2022-10-12 20:46:15 -0500

    Avance Actualizar Datos Usuario

--- src/pages/consultas/Consultas.js
+++ src/pages/consultas/Consultas.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 import "./Consultas.css";
 
 function Consultas() {
@@ -13,6 +14,26 @@ function Consultas() {
         return response;
     }
 
+    // /*4. agregamos otra constante al useState para actualizar el listado después de eliminar */
+    // const [upList,setUplist]=useState([false]);
+
+    // /*5. agregamos otra constante al useState para actualizar el estado del modal */
+    // const [show,setShow]=useState(false);
+
+    // const handleClose=()=>{setShow(false);}
+    // const handleOpen=()=>{setShow(true);}
+
+    // /*6. Estado para obtener los datos de cada registro a editar*/
+    // const [dataModal, setDataModal] = useState({})
+
+
+    // const handleChangeModal=({target})=>{
+    //     setDataModal({
+    //         ...dataModal,
+    //         [target.name]: target.value
+    //     })
+    // }
+
     const [list, setList] = useState([]);
 
     useEffect(() => {
@@ -22,47 +43,83 @@ function Consultas() {
         })
     }, [])
 
+    const [abrirModal, setAbrirModal] = useState(false)
+
+    function Modal() {
+        if (abrirModal === false) {
+            setAbrirModal(true)
+        }
+        else if (abrirModal === true) {
+            setAbrirModal(false)
+        }
+    }
+
+
     return (
         <body className='const-cont'>
-        <div className='container-const'>
-            {
-                list.map((list, index) => (
-                    <Table table hover variant='light'>
-                        <thead class="table-primary">
-                            <tr>
-                                <td>Número De Documento</td>
-                                <td>Tipo De Documento</td>
-                                <td>Nombre</td>
-                                <td>Apellido</td>
-                                <td>Fecha De Nacimiento</td>
-                                <td>Género</td>
-                                <td>Correo</td>
-                                <td>Teléfono</td>
-                                <td>País De Origen</td>
-                                <td>Contraseña</td>
-                                <td>Tipo De Usuario</td>
-                                <td>Imagen</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{list._id}</td>
-                                <td>{list.tipodoc}</td>
-                                <td>{list.nombre}</td>
-                                <td>{list.apellido}</td>
-                                <td>{list.fnacimiento}</td>
-                                <td>{list.genero}</td>
-                                <td>{list.email}</td>
-                                <td>{list.telefono}</td>
-                                <td>{list.paisorigen}</td>
-                                <td>{list.password}</td>
-                                <td>{list.tipouser}</td>
-                                <td>{list.img}</td>
-                            </tr>
-                        </tbody>
-                    </Table>))
-            }
-        </div>
+            <div className='container-const'>
+                <Table striped hover >
+                    <thead class="bg-info">
+                        <tr>
+                            <td>Número De Documento</td>
+                            <td>Tipo De Documento</td>
+                            <td>Nombre</td>
+                            <td>Apellido</td>
+                            <td>Fecha De Nacimiento</td>
+                            <td>Género</td>
+                            <td>Correo</td>
+                            <td>Teléfono</td>
+                            <td>País De Origen</td>
+                            <td>Contraseña</td>
+                            <td>Tipo De Usuario</td>
+                            <td>Imagen</td>
+                            <td>Acción</td>
+                        </tr>
+                    </thead>
+                    {
+                        list.map((list, index) => (
+                            <tbody class="bg-light">
+                                <tr>
+                                    <td>{list._id}</td>
+                                    <td>{list.tipodoc}</td>
+                                    <td>{list.nombre}</td>
+                                    <td>{list.apellido}</td>
+                                    <td>{list.fnacimiento}</td>
+                                    <td>{list.genero}</td>
+                                    <td>{list.email}</td>
+                                    <td>{list.telefono}</td>
+                                    <td>{list.paisorigen}</td>
+                                    <td>{list.password}</td>
+                                    <td>{list.tipouser}</td>
+                                    <td>{list.img}</td>
+                                    <td>
+                                        <Button variant='success' onClick={() => { Modal() }}><i class="fa-solid fa-pen"></i> Editar</Button>
+                                        <Button variant='danger'><i class="fa-solid fa-trash"></i> Eliminar</Button>
+                                    </td>
+                                </tr>
+                            </tbody>))
+                    }
+                </Table>
+            </div>
+
+            <div className={abrirModal ? 'modal-consult abrir' : abrirModal === false ? 'modal-consult outside-modal' : 'modal-consult'}>
+                <div class="contenidoModal">
+                    <span class="cerrarModal" onClick={() => { Modal() }}><i class="fa-solid fa-xmark"></i></span>
+                    <h2>Actualizar Datos</h2>
+
+                    <div class="mb-3">
+                        <label for="" class="form-label">Digite El Lado Del Cuadrado</label>
+                        <input type="number" class="form-control" id="lc"
+                            aria-placeholder="Ingrese el valor del lado del cuadrado" />
+                    </div>
+
+                    <center>
+                        <button type="submit" class="btn btn-success"><i class="fa-solid fa-user-pen"></i> Actualizar</button>
+                    </center>
+
+                </div>
+            </div>
+
         </body>
     );
 }
